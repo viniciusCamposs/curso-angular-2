61426e3e594daa209bb8adc75b19050cde61aa68
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-10-25 19:57:12 -0300

    Aulas iniciais

---
+++ hello-typescript/main.js
@@ -0,0 +1,16 @@
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+//ES 6 OU ES 2015
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+//Arrow function
+numeros.map(function (valor) { return valor * 2; }); //ES 2015 
+//tipagem de variavel
+var n1 = 'sfasf';
+n1 = 4;
---
+++ hello-typescript/main.ts
@@ -0,0 +1,20 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+//ES 6 OU ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor){
+            return valor * 2; 
+})
+
+//Arrow function
+numeros.map(valor => valor * 2); //ES 2015 
+
+//tipagem de variavel
+var n1 = 'sfasf';
