7e0f361731569554971d9b544a72bbd2b0cd3bb2
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-10-31 14:24:20 -0300

    Aula 15

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+  valor: number = 5;
+
+  mudarValor(){
+    this.valor++;
+  }
+
+  deletarCiclo: boolean = false;
+
+  destruirCiclo(){
+    this.deletarCiclo = true;
+  }
 }
