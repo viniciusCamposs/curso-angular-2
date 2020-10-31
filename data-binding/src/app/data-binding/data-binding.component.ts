d69c8f8ef4619b75f83f7a61d3678418f64443e5
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-10-31 12:03:31 -0300

    Aula 11 e 12

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight{
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `
+  ]
 })
 export class DataBindingComponent {
 
@@ -11,12 +19,39 @@ export class DataBindingComponent {
   cursoAngular: boolean = true;
   urlImagem: string = 'http://lorempixel.com/400/200/nature/'
 
+  valorAtual: string = '';
+  valorSalvo
+  isMouseOver: boolean = false;
+
+  nome: String = 'abc';
+
+  pessoa: any = {
+    nome: 'def',
+    idade: 12
+  };
+
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('Botao Clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor(){
