f0e5e0d2bc19982bc3dabf727161aec3375adda3
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-11-01 12:14:37 -0300

    Aula 22

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponent } from './teste/teste.component';
-import { Teste2Component } from './teste2/teste2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     TesteComponent,
-    Teste2Component
   ],
   imports: [
     BrowserModule,
