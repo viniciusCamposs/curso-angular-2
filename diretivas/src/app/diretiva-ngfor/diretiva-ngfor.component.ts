e0c5a0a6e128e20528ec1db3d96a63a05dd13b90
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2020-11-04 18:31:42 -0300

    Aula 28

--- diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
+++ diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-diretiva-ngfor',
   templateUrl: './diretiva-ngfor.component.html',
   styleUrls: ['./diretiva-ngfor.component.css']
 })
-export class DiretivaNgforComponent implements OnInit {
+export class DiretivaNgforComponent{
 
   cursos: string[] = ["Angular 2", "Java", "C#"];
+  
   constructor() { }
 
-  ngOnInit(): void {
-
-  }
 
 }
