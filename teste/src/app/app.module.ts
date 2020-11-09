f401586e924b5eb6be0b5c1614285b7b151507f5
unknown <vinicius.campos5@fatec.sp.gov.br>
2020-11-09 11:59:49 -0300

    Aula 33,34 e 35

---
+++ teste/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
+import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
+import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
+import { HighlightDirective } from './shared/highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FundoAmareloDirective,
+    DiretivasCustomizadasComponent,
+    HighlightMouseDirective,
+    HighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ teste/src/app/shared/fundo-amarelo.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, Renderer } from '@angular/core';
+
+@Directive({
+  selector: 'button[FundoAmarelo]'
+})
+export class FundoAmareloDirective {
+
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer
+    ) { 
+    // console.log(this.elementRef);
+    // this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+      this.renderer.setElementStyle(
+        this.elementRef.nativeElement,
+        'background-color',
+        'yellow')
+  }
+}
