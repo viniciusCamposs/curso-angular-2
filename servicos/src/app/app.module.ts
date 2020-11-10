784d10b3be1eebf64d2b314de5e4ddf027c9eff3
unknown <vinicius.campos5@fatec.sp.gov.br>
2020-11-10 10:05:54 -0300

    Aula 43

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { CursoModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule} from './criar-curso/criar-curso.module';
-
+import { LogService } from './shared/log.service';
 
 
 @NgModule({
@@ -22,7 +22,8 @@ import { CriarCursoModule} from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursoModule
   ],
-  providers: [CursosService],
+  providers: [LogService],
+ // providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from "@angular/core";
+import { LogService} from '../shared/log.service';
 
 @Injectable() 
 export class CursosService {
@@ -9,15 +10,17 @@ export class CursosService {
 
     private cursos: string[] = ['Angular 2', 'Java', 'Phonegap'];
 
-    constructor(){
+    constructor(private logService: LogService){
         console.log('CursosService');
     }
 
     getCursos(){
+        this.logService.consoleLog('Obtendo lista de cursos');
         return this.cursos;
     }
 
     addCursos(curso: string){
+        this.logService.consoleLog(`Criando um novo curso: ${curso}`)
         this.cursos.push(curso);
         this.emitirCursoCriado.emit(curso);
         CursosService.criouNovoCurso.emit(curso);
