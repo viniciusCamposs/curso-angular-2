d5bd71c042c4aa80ad54d2ff57bfec93656d9e62
Vinicius <vinicius.campos@mkdata.com.br>
2020-11-27 11:34:03 -0300

    Aula 58

---
+++ rotas-atualizado/src/app/cursos/cursos.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosComponent } from './cursos.component';
+import { CursosService } from './cursos.service';
+
+@NgModule({
+        imports: [
+            CommonModule,
+            RouterModule
+        ],
+        exports: [],
+        declarations: [
+            CursosComponent,
+            CursoDetalheComponent,
+            CursoNaoEncontradoComponent
+        ],
+        providers: [CursosService]
+})
+
+export class CursosModule { } 
