bd5c94e6056238db5e5468bb70b9cf667fbab856
Vinicius <vinicius.campos@mkdata.com.br>
2020-11-27 11:42:16 -0300

    Aula 59

--- rotas-atualizado/src/app/app-routing.module.ts
+++ rotas-atualizado/src/app/app-routing.module.ts
@@ -1,23 +1,20 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component'
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+
 
 const appRoutes: Routes = [
-  { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent },
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent }
 ];
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- rotas-atualizado/src/app/cursos/cursos.module.ts
+++ rotas-atualizado/src/app/cursos/cursos.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
+import { CursosRoutingModule } from './cursos.routing.module';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos.component';
@@ -10,7 +10,7 @@ import { CursosService } from './cursos.service';
 @NgModule({
         imports: [
             CommonModule,
-            RouterModule
+            CursosRoutingModule
         ],
         exports: [],
         declarations: [
