eed464459998e9339f36ed7732b2be2b09c83ca3
Vinicius <vinicius.campos@mkdata.com.br>
2020-12-03 09:44:18 -0300

    Aula 70

--- rotas-atualizado/src/app/app-routing.module.ts
+++ rotas-atualizado/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
-import { AlunosGuard } from './guards/alunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 
 const appRoutes: Routes = [
@@ -23,7 +23,9 @@ const appRoutes: Routes = [
   },
 
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] }
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: PaginaNaoEncontradaComponent}
 ];
 
 
--- rotas-atualizado/src/app/app.module.ts
+++ rotas-atualizado/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { FormsModule }   from '@angular/forms';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/alunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
