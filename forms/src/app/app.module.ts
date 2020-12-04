3832573ee48f442e3b32d8120e3981fdf817f6f5
Vinicius <vinicius.campos@mkdata.com.br>
2020-12-04 08:32:27 -0300

    Aula 76

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -5,21 +5,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AlertModule } from 'ngx-bootstrap/alert';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AlertModule.forRoot(),
-    FormsModule
+    FormsModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
