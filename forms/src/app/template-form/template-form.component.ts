9c035fb311fd93abcb80a71f3ab713dfcc85e5f6
Vinicius <vinicius.campos@mkdata.com.br>
2020-12-04 08:55:04 -0300

    Aula 79

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Loaine',
-    email: 'loiane@email.com'
+    nome: null,
+    email: null
   }
 
    onSubmit(form){
