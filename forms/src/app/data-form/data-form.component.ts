06f4cb63871efbc27905fd996b0e4732d5b492f7
Vinicius <vinicius.campos@mkdata.com.br>
2020-12-08 12:00:19 -0300

    Inicio da aula 91

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -12,9 +12,21 @@ export class DataFormComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    /*this.formulario = new FormGroup({
+      nome: new FormControl(null),
+      email: new FormControl(null),
+    });*/
+
+    this.formulario = this.formBuilder.group({
+      nome: [null],
+      email: [null]
+    })
+
+
   }
 
 }
