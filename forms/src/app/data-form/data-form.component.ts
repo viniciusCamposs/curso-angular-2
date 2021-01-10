e729bc4142efa59e24062ac9c2c410c56e7aed45
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2021-01-10 15:14:59 -0300

    Aula 97

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -20,18 +20,24 @@ export class DataFormComponent implements OnInit {
     /*this.formulario = new FormGroup({
       nome: new FormControl(null),
       email: new FormControl(null),
+      endereco: new FormGroup({
+        cep: new FormControl(null),...
+      })
     });*/
 
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
       email: [null, [Validators.required, Validators.email]],
-      cep:[null, Validators.required],
-      numero:[null, Validators.required],
-      complemento: [null],
-      rua: [null, Validators.required],
-      bairro: [null, Validators.required],
-      cidade: [null, Validators.required],
-      estado:  [null, Validators.required]
+
+      endereco: this.formBuilder.group({
+        cep:[null, Validators.required],
+        numero:[null, Validators.required],
+        complemento: [null],
+        rua: [null, Validators.required],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado:  [null, Validators.required]
+      })
     })
   }
 
