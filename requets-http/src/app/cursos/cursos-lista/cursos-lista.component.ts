1576a0963109f10cb2df7c38615ce8fd07b6de23
Vinicius Alves <vinicius.campos5@fatec.sp.gov.br>
2021-01-19 20:02:31 -0300

    Aula 124

--- requets-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requets-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import { CursosListaService } from './../cursos.service';
 import { Curso } from './../curso';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { empty, Observable, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -14,6 +15,7 @@ export class CursosListaComponent implements OnInit {
  // cursos: Curso[];
 
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosListaService) { }
 
@@ -21,7 +23,18 @@ export class CursosListaComponent implements OnInit {
    // this.service.list()
       //.subscribe(dados => this.cursos = dados);
 
-      this.cursos$ = this.service.list();
+    this.onRefresh();
+  }
+
+  onRefresh(){
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
   }
 
 }
